d426f5f3051ba6a8999d0850aaa6d084bc5a4a5b
Rodney DAVERMANN <davermannrodney@gmail.com>
2020-09-02 22:58:14 -0400

    Add files via upload

---
+++ 3_3Chapter3.py
@@ -0,0 +1,12 @@
+# Getting the password
+password_1 = input('Enter your password:')
+
+#Store the required password
+password = 'prospero'
+
+# Determine whether the entered password is the good one
+if password_1 == password:
+    print('Password accepted!')
+else:
+    print('Sorry, you entered a wrong password!')
+    
